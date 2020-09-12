bbb4c0232f98f02bc8f246a57dcd45e81e89d863
nooch-fifo <58356156+nooch-fifo@users.noreply.github.com>
2020-09-12 12:45:42 -0400

    Create ALPHA_student.php

---
+++ ALPHA_student.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Online Exam User Login</title>
+	<link rel="stylesheet"type="text/css"href="style1.css">
+</head>
+<body>
+	<h1> Student Login Successful! </h1>
+	<!--<h3> (Test Not Available Yet) <h3>
+	-->
+
+<p><font size = "5"> <a href= "https://afsaccess1.njit.edu/~dmr56/student_take.php"> Take Exam </a> </font>
+<p><br><br><br><font size = "5"> <a href= "https://afsaccess1.njit.edu/~dmr56/view_grades.php"> View Grades </a> </font>
+<p><br><br><br><font size = "4"> <a href= "https://afsaccess1.njit.edu/~dmr56/ALPHA_front.php"> Logout </a> </font>
+
+
+
+
+</body>
+</html>
