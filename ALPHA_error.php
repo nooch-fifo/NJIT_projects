3debcc4018b9c141fbfa499ae3d7cc42e9bd3be4
nooch-fifo <58356156+nooch-fifo@users.noreply.github.com>
2020-09-12 12:47:17 -0400

    Create ALPHA_error.php

---
+++ ALPHA_error.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Online Exam User Login</title>
+	<link rel="stylesheet"type="text/css"href="style1.css">
+</head>
+
+<body>
+<script>
+	window.alert("Error! Invalid Login Credentials");
+	window.location.replace("ALPHA_front.php");
+</script>
+
+	<h1> Online Exam Login </h1>
+
+<!--<form action="back_to_mid.php" method="post">
+	<h2> User ID  : <input type="text" name="user_id" class="form-control" placeholder="Enter User ID"><br>
+	<h2> Password: <input type="password" name="password" class="form-control" placeholder="Enter Password"><br>
+	<h2> <input type="submit" value="Login">
+</form>
+-->
+
+</body>
+</html>
